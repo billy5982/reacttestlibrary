5bbac3fc8759cb1c018bcd5ca4488db8ac908a13
billy5982 <kuu0404@naver.com>
2023-09-02 22:31:59 +0900

    문자열로 변경

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Learn React');
+  // 단언 : 테스트의 성공과 실패를 반환 expect
+  // matcher : toBeInTheDocument
   expect(linkElement).toBeInTheDocument();
 });
